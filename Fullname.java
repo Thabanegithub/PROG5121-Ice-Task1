2056128269a633cad7644d3de57b9da9ba537eb7
Thabanegithub <167181529+Thabanegithub@users.noreply.github.com>
2024-04-16 10:41:40 +0200

    Add files via upload

---
+++ Fullname.java
@@ -0,0 +1,26 @@
+package fullname;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Fullname {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Declarations and initializations
+        String fullName = "Mpho Thabane";
+        String emailAddress = "Mphozapunctual@gmail.com";
+        String phoneNumber = "083 366 4216";
+        
+        // Print out codes on three separate lines
+        System.out.println("Full Name: " + fullName);
+        System.out.println("Email Address: " + emailAddress);
+        System.out.println("Phone Number: " + phoneNumber); 
+    }
+    
+}
